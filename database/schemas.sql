4bb9677920833552fc3523c74ac44ac76babae4d
BonfaceKilz <bonfacemunyoki@gmail.com>
2019-12-14 02:26:18 +0300

    Add `code` column to schema and make `date_created` a timestamp

--- database/schemas.sql
+++ database/schemas.sql
@@ -1,10 +1,12 @@
 CREATE TABLE `products` (
 `id` int(8) NOT NULL AUTO_INCREMENT,
+`code` varchar(8) NOT NULL,
 `product_name` varchar(255) NOT NULL,
-`code` varchar(6) NOT NULL,
+`description` varchar(500) NOT NULL,
+`quantity` varchar(6) NOT NULL,
 `image` text NOT NULL,
 `price` decimal(9,2) NOT NULL,
-`date_created` datetime DEFAULT NULL,
+`date_created` timestamp NULL,
 `date_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 PRIMARY KEY (`id`),
 UNIQUE KEY unique_code (`code`)
